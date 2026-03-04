e197c298237411b336270c4a9761fc8a3c9071c1
Mihai Palaghian <info@blueroger,de>
2026-03-04 18:32:50 +0100

    chore: PHPStan Level 8 + PHP CS Fixer + GitHub Actions Setup

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/src')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder($finder)
+;
